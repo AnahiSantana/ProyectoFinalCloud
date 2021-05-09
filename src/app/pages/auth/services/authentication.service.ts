ccbd87431d2eca13d07cdbe19b252ff81134e418
Ruvo99 <migruv5799@gmail.com>
2021-05-09 17:35:13 -0500

    Register users in DynamoDB

---
+++ src/app/pages/auth/services/authentication.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+
+  authURL = "https://y969wcvj3c.execute-api.us-east-1.amazonaws.com/TestingUsers/";
+
+  constructor(private http: HttpClient) {
+  }
+
+  showUsers(){
+    return this.http.get(this.authURL + "/users", {observe:"body", responseType:"json"});
+  }
+
+  postUserInDynamo(user:any){
+    console.log(user);
+    return this.http.post(this.authURL + "/users", user); 
+  }
+}
