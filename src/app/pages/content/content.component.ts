b0323a2057dc3fd6eb08780f819d3db7bc3a603c
Anahí <is715773@iteso.mx>
2021-05-09 21:36:36 -0500

    removed footer

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -11,6 +11,7 @@ export class ContentComponent implements OnInit {
   nombreProducto = "Caja grande";
   precio = "$79.99";
   descripcion = "Caja de gran tamaño con muchos artículos dentro de ella. Recomendada si vas a compartir con alguien más.";
+  urlToImage = "https://i2.wp.com/samooraisnacks.co.uk/wp-content/uploads/2020/07/20200724_170331_0000.png?fit=1880%2C1576&ssl=1";
   reviews = [];
   users = [];
 
@@ -28,16 +29,19 @@ export class ContentComponent implements OnInit {
     this.nombreProducto = "Caja grande";
     this.precio = "$79.99";
     this.descripcion = "Caja de gran tamaño con muchos artículos dentro de ella. Recomendada si vas a compartir con alguien más.";
+    this.urlToImage = "https://www.ororadio.com.mx/noticias/wp-content/uploads/2018/09/dulces-tipicos-mexicanos-1.jpg";
   }
   showCajaMediana() {
     this.nombreProducto = "Caja mediana";
     this.precio = "$59.99";
     this.descripcion = "Caja intermedia en tamaño, que puede ser tanto para compartir con una persona como para ti solo si es que tienes mucha hambre";
+    this.urlToImage = "https://magazine.velasresorts.com.mx/wp-content/uploads/2020/08/dulces-mexicanos.jpeg";
   }
   showCajaPeque() {
     this.nombreProducto = "Caja pequeña";
     this.precio = "$39.99";
     this.descripcion = "Esta caja es indicada si tienes ganas de un snack repentino y no sabes que pedir. Es la más pequeña de todas."
+    this.urlToImage = "https://i2.wp.com/www.confitexpoinforma.com/wp-content/uploads/2019/11/Cua%CC%81l-es-el-escenario-de-los-dulces-ti%CC%81picos-mexicanos-actualmente.jpg?fit=1548%2C871&ssl=1";
   }
 
   getReviews() {
